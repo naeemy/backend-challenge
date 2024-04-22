80ae1b73c73b7140074e041fa3a3b38837427df3
naeemy <me@naeemy.xyz>
2024-04-22 21:08:41 +0700

    solution2 v2

--- solution2.go
+++ solution2.go
@@ -3,116 +3,83 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 func solution2() {
 	fmt.Println(decode("LLRR="))
 	fmt.Println(decode("==RLL"))
 	fmt.Println(decode("=LLRR"))
+	fmt.Println(decode("=RRLL"))
 	fmt.Println(decode("RRL=R"))
 	fmt.Println(decode("RRRL="))
 }
 
-func findMin(list []int) int {
-	minimum := math.MaxInt64
+func decode(encoded string) string {
+	result := []int{}
 
-	for _, number := range list {
-		if number <= minimum {
-			minimum = number
-		}
+	switch encoded[0] {
+	case 'L':
+		result = append(result, 1, 0)
+	case 'R':
+		result = append(result, 0, 1)
+	default:
+		result = append(result, 0, 0)
 	}
 
-	return minimum
-}
+	// fmt.Println(result)
 
-func decode(encoded string) []int {
-	cursor := []int{}
-	current := 0
-	const CHANGE = 1
-
-	// init
-	firstChar := encoded[0]
-	if firstChar == 'L' {
-		cursor = append(cursor, CHANGE, current)
-	} else if firstChar == 'R' {
-		cursor = append(cursor, current, CHANGE)
-		current += CHANGE
-	} else {
-		cursor = append(cursor, current, current)
-	}
-
-	// draw
 	for i := 1; i < len(encoded); i++ {
-		if encoded[i] == 'L' {
-			if encoded[i-1] == 'R' {
-				current = 0
-			} else {
-				current -= CHANGE
-			}
-			cursor = append(cursor, current)
-		} else if encoded[i] == 'R' {
-			current += CHANGE
-			cursor = append(cursor, current)
-		} else {
-			cursor = append(cursor, current)
-		}
-	}
+		code := encoded[i]
 
-	adjust(encoded, &cursor)
-	return cursor
-}
+		if code == 'L' {
+			toBeAppended := 0
+			firstL := -1
 
-func adjust(encoded string, cursor *[]int) {
-
-	// increase
-	toAdjust := int(math.Abs(float64(findMin(*cursor) - 0)))
-	for i := 0; i < len(encoded); i++ {
-		if i == 1 {
-			if encoded[i-1] == '=' {
-				(*cursor)[0] = (*cursor)[i]
-			} else if encoded[i-1] == 'L' {
-				(*cursor)[0] = (*cursor)[i] + 1
-			}
-		}
-
-		(*cursor)[i+1] += toAdjust
-	}
-
-	// check starting with '='
-	if encoded[0] == '=' {
-		temp := []int{0, 0}
-		for i := 1; i < len(encoded); i++ {
-			if encoded[i] == '=' {
-				temp = append(temp, 0)
-			} else if encoded[i] == 'L' && len(temp) > 2 {
-				if sum(temp) == 0 {
-					for j := range temp {
-						temp[j] = (*cursor)[i+1]
+			for j := len(result) - 1; j >= 1; j-- {
+				if result[j] <= result[j-1] {
+					if math.Abs(float64(result[j]-result[j-1])) > 1 {
+						firstL = j
+					} else {
+						firstL = j - 1
 					}
-				}
-			} else if encoded[i] == 'R' && len(temp) > 2 {
-				if sum(temp) == 0 {
-					for j := range temp {
-						temp[j] = 0
-					}
+				} else if result[j] > result[j-1] {
+					break
+				} else {
+					toBeAppended = 1
 				}
 			}
 
-			if len(temp) > 2 && (encoded[i] == 'R' || encoded[i] == 'L') {
-				newCursor := append(temp, (*cursor)[len(temp):]...)
-				copy(*cursor, newCursor)
-				break
+			if firstL > -1 {
+				for k := firstL; k < len(result); k++ {
+					result[k] += 1
+				}
 			}
 
+			result = append(result, toBeAppended)
+
+		} else if code == 'R' {
+
+			for j := len(result) - 1; j >= 1; j-- {
+				if result[j] < result[j-1] {
+					break
+				}
+			}
+
+			result = append(result, result[len(result)-1]+1)
+		} else if code == '=' {
+			for j := len(result) - 1; j >= 1; j-- {
+				if result[j] < result[j-1] || result[j] > result[j-1] {
+					break
+				} else {
+					result[j-1] = result[j]
+				}
+			}
+
+			result = append(result, result[len(result)-1])
 		}
-	}
-}
 
-func sum(numbers []int) int {
-	total := 0
-	for _, number := range numbers {
-		total += number
 	}
 
-	return total
+	return strings.Join(strings.Split(fmt.Sprint(result), " "), "")
 }
