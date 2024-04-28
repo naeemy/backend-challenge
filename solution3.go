805181913965cae818409681510550bfc45eb63c
naeemy <me@naeemy.xyz>
2024-04-28 13:30:43 +0700

    rough solution3

---
+++ solution3.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+func solution3() {
+	menu := make(map[string]int)
+	texts := parseAPI("https://baconipsum.com/api/?type=meat-and-filler&paras=99&format=text")
+
+	re := regexp.MustCompile(`(\s|,|\.)+`)
+	cleanTexts := re.ReplaceAllString(texts, ` `)
+
+	for _, word := range strings.Split(cleanTexts, " ") {
+		if len(word) > 0 {
+			if menu[word] == 0 {
+				menu[word] = 1
+			} else {
+				menu[word] += 1
+			}
+		}
+	}
+
+	jsonMenu, _ := json.MarshalIndent(menu, "", "")
+
+	fmt.Println(string(jsonMenu))
+}
+
+func parseAPI(url string) string {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	content, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(content)
+}
